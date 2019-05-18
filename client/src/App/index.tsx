90abf8abb3cbfd804242e130b49cd27cfe0ec937
Eugeny Schibrikov <eugeny.schibrikov@paxport.net>
2019-05-19 00:32:50 +0500

    prettier format

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,103 +1,106 @@
 /* eslint-disable react/style-prop-object */
-import React, {useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import ReactMapboxGl, { Layer, Feature, ZoomControl } from "react-mapbox-gl";
-import {MapMouseEvent} from "mapbox-gl";
+import { MapMouseEvent } from "mapbox-gl";
 import config from "../config";
-import {EventEditor} from "../EventEditor";
+import { EventEditor } from "../EventEditor";
 
 type MapCoords = [number, number];
 
 const Map = ReactMapboxGl({
-    accessToken: config.mapboxToken
+  accessToken: config.mapboxToken
 });
 
 export interface ChilliumEvent {
-    name: string,
-    description: string,
-    location: {
-        latitude: number,
-        longitude: number
-    },
+  name: string;
+  description: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
 }
 
 export default function Index() {
-    const [userPosition, setUserPosition] = useState<MapCoords>([ 0, 0 ]);
-    const [eventEditorOpened, setEventEditorOpened] = useState(false);
-    const [selectedPosition, setSelectedPosition] = useState<MapCoords>([0, 0]);
-    const [nearEvents, setNearEvents] = useState<ChilliumEvent[]>([]);
+  const [userPosition, setUserPosition] = useState<MapCoords>([0, 0]);
+  const [eventEditorOpened, setEventEditorOpened] = useState(false);
+  const [selectedPosition, setSelectedPosition] = useState<MapCoords>([0, 0]);
+  const [nearEvents, setNearEvents] = useState<ChilliumEvent[]>([]);
 
-    console.log(nearEvents);
+  console.log(nearEvents);
 
-    useEffect(() => {
-        const savePosition = (position: Position) => {
-            setUserPosition([ position.coords.longitude, position.coords.latitude ])
-        };
+  useEffect(() => {
+    const savePosition = (position: Position) => {
+      setUserPosition([position.coords.longitude, position.coords.latitude]);
+    };
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(savePosition);
-            const id = navigator.geolocation.watchPosition(savePosition);
-            return () => navigator.geolocation.clearWatch(id);
-        }
-    }, []);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(savePosition);
+      const id = navigator.geolocation.watchPosition(savePosition);
+      return () => navigator.geolocation.clearWatch(id);
+    }
+  }, []);
 
-    useEffect(() => {
-        // fetch(`/api/events?lat=${userPosition[1]}&lng=${userPosition[0]}`)
-        //     .then(resp => resp.json())
-        //     .then((events: ChilliumEvent[]) => setNearEvents(events));
-    }, [userPosition]);
+  useEffect(() => {
+    // fetch(`/api/events?lat=${userPosition[1]}&lng=${userPosition[0]}`)
+    //     .then(resp => resp.json())
+    //     .then((events: ChilliumEvent[]) => setNearEvents(events));
+  }, [userPosition]);
 
-    const selectPosition = useCallback((map, event) => {
-        const mapEvent = event as MapMouseEvent;
+  const selectPosition = useCallback((map, event) => {
+    const mapEvent = event as MapMouseEvent;
 
-        setEventEditorOpened(true);
-        setSelectedPosition([ mapEvent.lngLat.lng, mapEvent.lngLat.lat ]);
-    }, []);
+    setEventEditorOpened(true);
+    setSelectedPosition([mapEvent.lngLat.lng, mapEvent.lngLat.lat]);
+  }, []);
 
-    return (
-        <div>
-            <Map
-                style="mapbox://styles/mapbox/streets-v9"
-                containerStyle={{
-                    height: "100vh",
-                    width: "100vw"
-                }}
-                center={userPosition}
-                zoom={[15]}
-                onClick={selectPosition}
-                movingMethod="jumpTo"
-            >
-                <ZoomControl />
-                <Layer
-                    type="circle"
-                    paint={{
-                        'circle-radius': 5,
-                        'circle-color': '#E54E52',
-                        'circle-opacity': 0.8,
-                        'circle-stroke-width': 1,
-                        'circle-stroke-color': '#000'
-                    }}
-                >
-                    <Feature coordinates={userPosition} />
-                </Layer>
-                <Layer
-                    type="circle"
-                    paint={{
-                        'circle-radius': 8,
-                        'circle-color': '#0000FF',
-                        'circle-opacity': 1
-                    }}
-                >
-                    {
-                        nearEvents.map(event => (
-                            <Feature coordinates={[ event.location.longitude, event.location.latitude ]} />
-                        ))
-                    }
-                </Layer>
-            </Map>
-            {
-                eventEditorOpened &&
-                    <EventEditor position={selectedPosition} onClose={() => setEventEditorOpened(false)} setEvents={setNearEvents} />
-            }
-        </div>
-    )
+  return (
+    <div>
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+        center={userPosition}
+        zoom={[15]}
+        onClick={selectPosition}
+        movingMethod="jumpTo"
+      >
+        <ZoomControl />
+        <Layer
+          type="circle"
+          paint={{
+            "circle-radius": 5,
+            "circle-color": "#E54E52",
+            "circle-opacity": 0.8,
+            "circle-stroke-width": 1,
+            "circle-stroke-color": "#000"
+          }}
+        >
+          <Feature coordinates={userPosition} />
+        </Layer>
+        <Layer
+          type="circle"
+          paint={{
+            "circle-radius": 8,
+            "circle-color": "#0000FF",
+            "circle-opacity": 1
+          }}
+        >
+          {nearEvents.map(event => (
+            <Feature
+              coordinates={[event.location.longitude, event.location.latitude]}
+            />
+          ))}
+        </Layer>
+      </Map>
+      {eventEditorOpened && (
+        <EventEditor
+          position={selectedPosition}
+          onClose={() => setEventEditorOpened(false)}
+          setEvents={setNearEvents}
+        />
+      )}
+    </div>
+  );
 }
--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,4 @@
 export default {
-    mapboxToken: "pk.eyJ1Ijoic2hvb2siLCJhIjoiY2p2dGJkOWNuMWNiZjRhb3oyMHB2ZmhjdyJ9.J1u2GpeX6HyMdqojkCFh2A",
-}
+  mapboxToken:
+    "pk.eyJ1Ijoic2hvb2siLCJhIjoiY2p2dGJkOWNuMWNiZjRhb3oyMHB2ZmhjdyJ9.J1u2GpeX6HyMdqojkCFh2A"
+};
