c0d8010e892bca6fa75c4871e18bc20b940ead5a
IlyaAgarishev <ilyagarishev@gmail.com>
2019-05-18 20:08:55 +0500

    EventWindow

--- client/src/App/index.tsx
+++ client/src/App/index.tsx
@@ -1,68 +1,69 @@
 /* eslint-disable react/style-prop-object */
-import React, {useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import ReactMapboxGl, { Layer, Feature, ZoomControl } from "react-mapbox-gl";
-import {MapMouseEvent} from "mapbox-gl";
-import styles from './index.module.css';
+import { MapMouseEvent } from "mapbox-gl";
+import styles from "./index.module.css";
 import config from "../config";
-import {EventEditor} from "../EventEditor";
+import { EventEditor } from "../EventEditor";
+import EvetnWindow from "../EventWindow";
+import EventWindow from "../EventWindow";
 
 type MapCoords = [number, number];
 
 const Map = ReactMapboxGl({
-    accessToken: config.mapboxToken
+  accessToken: config.mapboxToken
 });
 
 export default function Index() {
-    const [userPosition, setUserPosition] = useState<MapCoords>([ 0, 0 ]);
-    const [eventEditorOpened, setEventEditorOpened] = useState(false);
-    const [selectedPosition, setSelectedPosition] = useState<MapCoords>([0, 0]);
+  const [userPosition, setUserPosition] = useState<MapCoords>([0, 0]);
+  const [eventEditorOpened, setEventEditorOpened] = useState(false);
+  const [selectedPosition, setSelectedPosition] = useState<MapCoords>([0, 0]);
 
-    useEffect(() => {
-        const savePosition = (position: Position) => {
-            setUserPosition([ position.coords.longitude, position.coords.latitude ])
-        };
+  useEffect(() => {
+    const savePosition = (position: Position) => {
+      setUserPosition([position.coords.longitude, position.coords.latitude]);
+    };
 
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(savePosition);
-            const id = navigator.geolocation.watchPosition(savePosition);
-            return () => navigator.geolocation.clearWatch(id);
-        }
-    }, []);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(savePosition);
+      const id = navigator.geolocation.watchPosition(savePosition);
+      return () => navigator.geolocation.clearWatch(id);
+    }
+  }, []);
 
-    const selectPosition = useCallback((map, event) => {
-        console.log(event);
-        const mapEvent = event as MapMouseEvent;
+  const selectPosition = useCallback((map, event) => {
+    console.log(event);
+    const mapEvent = event as MapMouseEvent;
 
-        setEventEditorOpened(true);
-        setSelectedPosition([ mapEvent.lngLat.lng, mapEvent.lngLat.lat ]);
-    }, []);
+    setEventEditorOpened(true);
+    setSelectedPosition([mapEvent.lngLat.lng, mapEvent.lngLat.lat]);
+  }, []);
 
-    return (
-        <div>
-            <Map
-                style="mapbox://styles/mapbox/streets-v9"
-                containerStyle={{
-                    height: "100vh",
-                    width: "100vw"
-                }}
-                center={userPosition}
-                zoom={[15]}
-                onClick={selectPosition}
-                movingMethod="jumpTo"
-            >
-                <ZoomControl />
-                <Layer
-                    type="symbol"
-                    id="marker"
-                    layout={{ "icon-image": "marker-15" }}
-                >
-                    <Feature coordinates={userPosition} />
-                </Layer>
-            </Map>
-            {
-                eventEditorOpened &&
-                    <EventEditor position={selectedPosition} onClose={() => setEventEditorOpened(false)} />
-            }
-        </div>
-    )
+  return (
+    <div>
+      <EventWindow />
+      <Map
+        style="mapbox://styles/mapbox/streets-v9"
+        containerStyle={{
+          height: "100vh",
+          width: "100vw"
+        }}
+        center={userPosition}
+        zoom={[15]}
+        onClick={selectPosition}
+        movingMethod="jumpTo"
+      >
+        <ZoomControl />
+        <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
+          <Feature coordinates={userPosition} />
+        </Layer>
+      </Map>
+      {eventEditorOpened && (
+        <EventEditor
+          position={selectedPosition}
+          onClose={() => setEventEditorOpened(false)}
+        />
+      )}
+    </div>
+  );
 }
