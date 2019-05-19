4dad818dd3700469a7f9abfa6e84f96f915b69e1
Eugeny Schibrikov <eugeny.schibrikov@paxport.net>
2019-05-19 13:12:15 +0500

    fixed endpoint

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  endpoint: "http://10.34.34.56:8080",
+  endpoint: "https://chillium.herokuapp.com",
   mapboxToken:
     "pk.eyJ1Ijoic2hvb2siLCJhIjoiY2p2dGJkOWNuMWNiZjRhb3oyMHB2ZmhjdyJ9.J1u2GpeX6HyMdqojkCFh2A",
   yandexToken: "58969dfd-fa1b-4983-8b66-bb6c4d96ae6f"
